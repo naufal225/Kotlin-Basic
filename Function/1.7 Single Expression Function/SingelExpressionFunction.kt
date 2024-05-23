8cdf061a7b5b09822c047198ce24ed514c32efb6
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-05-23 08:28:23 +0700

    SingelExpressionFunction.kt

---
+++ Function/1.7 Single Expression Function/SingelExpressionFunction.kt
@@ -0,0 +1,11 @@
+// Single Expression Function
+// pendeklarsian function dengan 1 baris saja
+
+fun tambah(a: Int, b: Int) : Int = a + b
+
+fun sapa(nama: String) : Unit = println("Halo, $nama")
+
+fun main() {
+    println(tambah(6,8))
+    sapa("Budi")
+}
